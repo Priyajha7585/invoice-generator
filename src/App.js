196e502ff6f4caa19619d48ef90aa8d77cf3fc0d
Priya_Jha <priyajha7585@gmail.com>
2022-08-17 23:32:09 +0530

    Updated invoice generator project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <main>
+        <header>
+          <div>
+            <h2>INVOICE GENERATOR</h2>
+          </div>
+          <div>
+            <ul>
+              <li>Print</li>
+              <li>Download</li>
+              <li>Send</li>
+            </ul>
+          </div>
+        </header>
+      </main>
+    </>
+  );
+}
+
+export default App;
