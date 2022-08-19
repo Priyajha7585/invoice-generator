7f0878551dbc5be664d93472e10fdb363cdd4813
Priya_Jha <priyajha7585@gmail.com>
2022-08-19 23:55:16 +0530

    Updated project invoice generator

--- src/App.js
+++ src/App.js
@@ -73,19 +73,7 @@ function App() {
           </ul>
         </footer>
         {/* End of Footer */}
-                {/* Footer */}
-                <footer>
-          <ul className="flex flex-wrap items-center justify-center">
-            <li><span className="font-bold">Your name :</span> Priya Jha</li>
-            <li><span className="font-bold">Your email :</span> priyajha7585@gmail.com</li>
-            <li><span className="font-bold">Phone number :</span> 7984256476</li>
-            <li><span className="font-bold">Bank :</span> Kotak bank</li>
-            <li><span className="font-bold">Account holder name :</span> Priya Jha</li>
-            <li><span className="font-bold">Account number :</span> 78328732763287</li>
-            <li><span className="font-bold">Website :</span> www.priyajha.com</li>
-          </ul>
-        </footer>
-        {/* End of Footer */}
+        
       </main>
     </>
   );
